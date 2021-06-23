6e4f8757b9dd77f0c89d876111aaf73d95363872
MarcioSchaefer <marciorschaefer@mx2.unisc.br>
2021-06-23 18:12:15 -0300

    incluido leitor de arquivo para entrada de dados

--- src/main/java/Interpretador/Lexico/ExecutaAnaliseLexica.java
+++ src/main/java/Interpretador/Lexico/ExecutaAnaliseLexica.java
@@ -1,14 +1,45 @@
 package Interpretador.Lexico;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Scanner;
 
 public class ExecutaAnaliseLexica {
-	
-	public static void main(String[] args) throws IOException {
-		String expressao = "Qual e o 3º elemento da serie [5, 10, 15, 20]?";
+	public static void main(String[] args) throws IOException  {
 		
-		AnalisadorLexico analisadorLexico = new AnalisadorLexico(new StringReader(expressao));
+		// Informamos o diretorio do caminho com a entrada de dados a ser analisada
+		File diretorioArquivo = new File("src/main/java/Interpretador/Lexico/EntradaDeDadosTXT.txt");
+		
+		// Variavel que ira guardar o arquivo 
+		StringBuilder arquivo = new StringBuilder();
+		
+		// Caso seja possivel abrir o arquivo, a variavel irá guardar cada linha presente no mesmo
+		try(Scanner leitorDeArquivo = new Scanner(diretorioArquivo)) {	
+			while (leitorDeArquivo.hasNextLine())
+				arquivo.append(leitorDeArquivo.nextLine().concat("\n")); 
+			
+		leitorDeArquivo.close(); 
+					
+		// Apos a correta leitura, ira apresentar os dados que foram lidos
+		System.out.println("----------------------------------------");
+		System.out.println("DADA A SEGUINTE ENTRADA DE DADOS:");
+		System.out.println("----------------------------------------\n\n");
+		System.out.println(arquivo.toString());
+		System.out.println("\n\n----------------------------------------");
+		System.out.println("FOI REALIZADO A SEGUINTE ANALISE LEXICA:");
+		System.out.println("----------------------------------------\n\n");
+		
+		// Passara os dados lidos para o analisador lexico fazer a analise
+		AnalisadorLexico analisadorLexico = new AnalisadorLexico(new StringReader(arquivo.toString()));
+		
+		// Analisador lexico retorna os dados analisados
 		analisadorLexico.yylex();
+		
+		// Casso nao encontre o arquivo de entrada de dados
+		} catch (FileNotFoundException e) {
+			System.out.println("O arquivo de entrada nao foi encontrado!");
+		} 
 	}
 }
--- src/main/java/Interpretador/Lexico/GeradorLexico.java
+++ src/main/java/Interpretador/Lexico/GeradorLexico.java
@@ -1,20 +1,15 @@
 package Interpretador.Lexico;
 
-import java.io.File;
-
 public class GeradorLexico {
 
 	public static void main(String[] args) {
 		
-		File diretorioRaiz = new File("");
-		String diretorioArquivo = "src/main/java/Interpretador/Lexico/Linguagem.flex";
+		// Variavel contendo diretorio onde se encontra o arquivo com as definições da linguagem a ser analisada	
+		String arquivo[] = {"src/main/java/Interpretador/Lexico/Linguagem.flex"};
 		
-		// Busca o arquivo com as definições da linguagem a ser analisada	
-		String arquivo[] = {diretorioRaiz + diretorioArquivo};
-			
-		// Gera o analisador léxico
+		// Gera o analisador léxico atraves do arquivo informado
 		jflex.Main.main(arquivo);
-			
+		
 		// Exibe mensagem de sucesso caso gere o analisador lexico
 		System.out.println("\n\nAnalisador Lexico criado com sucesso =)");
 	}
